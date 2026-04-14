c010810d2588b97d043a9e1c165ec64afbc55bb1
Mauricio Gonzalez <mauriciogonzalez1256@gmail.com>
2026-04-14 12:55:38 -0400

    Implemented Form to add Tareas

--- src/App.jsx
+++ src/App.jsx
@@ -18,17 +18,50 @@ function ListaTareas({tareas, filtroCategoria, filtroEstado, funcion}) {//Las ta
   //Ahora, para cada una de las tareas filtradas, retornamos un componente React, lo cual lo hacemos con un mapeo a la lista de una funcion que devuelve dicho componente para cada tarea
   
   return(
-
     <>
-    
       {tareasFiltradas.map((tarea) => {
-        return <tr key={tarea.id}><td>{tarea.id}</td><td>{tarea.texto}</td><td>{tarea.categoria}</td><td>{tarea.estado}</td><td><button onClick={() => (console.log(tarea.texto))}>Editar</button></td></tr>
+        return <tr key={tarea.id}><td>{tarea.texto}</td><td>{tarea.categoria}</td><td>{tarea.estado}</td><td><button onClick={() => (console.log(tarea.texto))}>Editar</button></td></tr>
       })}
-
     </>
-
-
   )
+
+}
+
+function FormularioTarea({nuevaTarea, setNuevaTarea, agregarTarea}) {
+
+  return(//Un formulario que cuando se hace submit llama a la funcion agregarTarea
+
+    <form onSubmit={agregarTarea}>
+
+
+
+    <table>
+      <thead>
+          <tr>
+            <th>Texto</th>
+            <th>Categoría</th>
+            <th>Estado</th>
+          </tr>
+        </thead>
+      <tbody>
+        <tr>
+          <td><label>Texto:</label><input type="text" value={nuevaTarea.texto} onChange={(e) => setNuevaTarea({...nuevaTarea, texto: e.target.value})} /></td> 
+          <td><label>Categoría:</label><select value={nuevaTarea.categoria} onChange={(e) => setNuevaTarea({...nuevaTarea, categoria: e.target.value})}>
+            <option value="personal">Personal</option>
+            <option value="trabajo">Trabajo</option>
+            <option value="estudio">Estudio</option>
+          </select></td>
+          <td><label>Estado:</label><select value={nuevaTarea.estado} onChange={(e) => setNuevaTarea({...nuevaTarea, estado: e.target.value})}>
+            <option value="pendiente">Pendiente</option>
+            <option value="completada">Completada</option>
+          </select></td>
+        </tr>
+      </tbody>
+    </table>
+    <button type="submit">Agregar Tarea</button>
+    </form>
+
+  )// Para cada input, hay un onChange, un estado que se guarda en el objeto e por cada evento (teclado por ejemplo) y que actualizar el valor de nuevaTarea
 
 }
 
@@ -49,7 +82,6 @@ function App() {
       <table>
         <thead>
           <tr>
-            <th>ID</th>
             <th>Texto</th>
             <th>Categoría</th>
             <th>Estado</th>
@@ -59,8 +91,14 @@ function App() {
           <ListaTareas tareas={tareas} filtroCategoria={filtroCategoria} filtroEstado={filtroEstado} /*funcion undefined for now*/ />
         </tbody>
       </table>
-      <></>
+
+      <FormularioTarea nuevaTarea={nuevaTarea} setNuevaTarea={setNuevaTarea} agregarTarea={(e) => { //Esta es la funcion llamada cuando se hace submit a Agregar Tarea
+        e.preventDefault(); // Evitar que el formulario se envíe en el evento actual recargue la página
+        setTareas([...tareas, { id: Date.now(), texto: nuevaTarea.texto, categoria: nuevaTarea.categoria, estado: 'pendiente' }]);
+        setNuevaTarea({ texto: '', categoria: 'personal' });
+      } } />
     </>
+
   )
 }
 
