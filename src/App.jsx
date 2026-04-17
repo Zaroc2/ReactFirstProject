25b0f541ad49ce4a0a8a2c84609a7bf0a8718350
Mauricio Gonzalez <mauriciogonzalez1256@gmail.com>
2026-04-17 18:12:42 -0400

    Agregado todo lo que faltaba, filtros, cancelar y la otra cosa que dije que no recuerdo, ademas de ponerle una fotito de zaroc en el icono

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import './App.css'
 
 
@@ -14,7 +14,10 @@ function ListaTareas({tareas, filtroCategoria, filtroEstado, funcion ,completeTa
   }
 
   //Ahora, para cada una de las tareas filtradas, retornamos un componente React, lo cual lo hacemos con un mapeo a la lista de una funcion que devuelve dicho componente para cada tarea
-  
+  //El colSpan="3" es para que el mensaje de "No hay tareas :)" ocupe 3 columnas de la tabla
+  if(tareasFiltradas.length === 0) {
+    return <tr><td colSpan="6">No hay tareas :)</td></tr>
+  }
   return(
     <>
       {tareasFiltradas.map((tarea) => {
@@ -51,13 +54,13 @@ function FormularioTarea({nuevaTarea, setNuevaTarea, agregarTarea}) {
         <tr>
           <td><label>Texto:</label><input type="text" value={nuevaTarea.texto} onChange={(e) => setNuevaTarea({...nuevaTarea, texto: e.target.value})} /></td> 
           <td><label>Categoría:</label><select value={nuevaTarea.categoria} onChange={(e) => setNuevaTarea({...nuevaTarea, categoria: e.target.value})}>
-            <option value="personal">Personal</option>
-            <option value="trabajo">Trabajo</option>
-            <option value="estudio">Estudio</option>
+            <option value="Personal">Personal</option>
+            <option value="Trabajo">Trabajo</option>
+            <option value="Estudio">Estudio</option>
           </select></td>
           <td><label>Estado:</label><select value={nuevaTarea.estado} onChange={(e) => setNuevaTarea({...nuevaTarea, estado: e.target.value})}>
-            <option value="pendiente">Pendiente</option>
-            <option value="completada">Completada</option>
+            <option value="Pendiente">Pendiente</option>
+            <option value="Completada">Completada</option>
           </select></td>
         </tr>
       </tbody>
@@ -71,28 +74,51 @@ function FormularioTarea({nuevaTarea, setNuevaTarea, agregarTarea}) {
 
 
 //Componente para editar la tarea
-function ModalEditarTarea({tarea, actualizarYCerrarModal}) {
+function ModalEditarTarea({tarea, actualizarYCerrarModal,cerrarModal}) {
 
   const [tareaEditada, setTareaEditada] = useState(tarea); // Estado para la tarea editada
 
   return (
     <div className="modalBackground">
       <div className="modalBody">
-        <h2>Editar Tarea</h2>
+        <h2>Editar "{tarea.texto}"</h2>
+        <div className="formRow">
+          <label>Texto:</label>
         <input type="text" value={tareaEditada.texto} onChange={(e) => setTareaEditada({...tareaEditada, texto: e.target.value})} />
-        <select value={tareaEditada.categoria} onChange={(e) => setTareaEditada({...tareaEditada, categoria: e.target.value})}>
-          <option value="personal">Personal</option>
-          <option value="trabajo">Trabajo</option>
-          <option value="estudio">Estudio</option>
-        </select>
-        <select value={tareaEditada.estado} onChange={(e) => setTareaEditada({...tareaEditada, estado: e.target.value})}>
-          <option value="pendiente">Pendiente</option>
-          <option value="completada">Completada</option>
-        </select>
+        </div>
+        <div className="formRow">
+          <label>Categoría:</label>
+          <select value={tareaEditada.categoria} onChange={(e) => setTareaEditada({...tareaEditada, categoria: e.target.value})}>
+            <option value="Personal">Personal</option>
+            <option value="Trabajo">Trabajo</option>
+            <option value="Estudio">Estudio</option>
+          </select>
+        </div>
+        <div className="formRow">
+          <label>Estado:</label>
+          <select value={tareaEditada.estado} onChange={(e) => setTareaEditada({...tareaEditada, estado: e.target.value})}>
+            <option value="Pendiente">Pendiente</option>
+            <option value="Completada">Completada</option>
+          </select>
+        </div>
         <button onClick={() => actualizarYCerrarModal(tareaEditada)}>Actualizar</button>
+        <button onClick={() => cerrarModal()}>Cancelar</button>
       </div>
     </div>
   )
+
+}
+
+function cargarTareasLocalStorage(){
+
+  const tareasGuardadas = localStorage.getItem('tareas');
+
+  if(tareasGuardadas !== null) {
+    return JSON.parse(tareasGuardadas);
+  } else {
+    return [ { id: 1, texto: 'Tarea de prueba 1', categoria: 'Personal', estado: 'Pendiente' },
+             { id: 2, texto: 'Tarea de prueba 2', categoria: 'Trabajo', estado: 'Completada' } ];
+  }
 
 }
 
@@ -100,18 +126,17 @@ function ModalEditarTarea({tarea, actualizarYCerrarModal}) {
 
 function App() {
 
-  const [tareas, setTareas] = useState([
-    { id: 1, texto: 'Tarea de prueba 1', categoria: 'personal', estado: 'pendiente' },
-    { id: 2, texto: 'Tarea de prueba 2', categoria: 'trabajo', estado: 'completada' }
-  ]); // Estado para almacenar las tareas con 2 tareas de prueba
-  const [nuevaTarea, setNuevaTarea] = useState({ texto: '', categoria: 'personal' }); // Estado para la nueva tarea
+  const [tareas, setTareas] = useState(cargarTareasLocalStorage()); // Estado para almacenar las tareas con 2 tareas de prueba
+  const [nuevaTarea, setNuevaTarea] = useState({id: null, texto: '', categoria: 'Personal' , estado: 'Pendiente'}); // Estado para la nueva tarea
   const [filtroCategoria, setFiltroCategoria] = useState('todas'); // Estado para el filtro de categoría
   const [filtroEstado, setFiltroEstado] = useState('todas'); // Estado para el filtro de estado
   const [tareaAEditar, setTareaAEditar] = useState(null); // Estado para la tarea que se está editando
   const [modalAbierto, setModalAbierto] = useState(false); // Estado para controlar la apertura del modal
 
+  useEffect(() => {localStorage.setItem('tareas', JSON.stringify(tareas))}, [tareas]); // Esto realiza un efecto secundario, cada vez que [tareas] cambia, realiza el localStorage.setItem()
+
   function completeTask(tarea){
-    setTareas(tareas.map(t => t.id === tarea.id ? {...t, estado: t.estado === 'pendiente' ? 'completada' : 'pendiente'} : t));
+    setTareas(tareas.map(t => t.id === tarea.id ? {...t, estado: t.estado === 'Pendiente' ? 'Completada' : 'Pendiente'} : t));
   }
 
   function deleteTask(tarea){
@@ -138,8 +163,25 @@ function App() {
     cerrarModal();
   }
 
-  return (//El colSpan="3" es para que el mensaje de "No hay tareas :)" ocupe 3 columnas de la tabla
+  return (
     <>
+    <div className="filtros">
+      <label>Categoría:</label>
+      <select value={filtroCategoria} onChange={(e) => setFiltroCategoria(e.target.value)}>
+        <option value="todas">Todas</option>
+        <option value="Personal">Personal</option>
+        <option value="Trabajo">Trabajo</option>
+        <option value="Estudio">Estudio</option>
+      </select>
+    </div>
+    <div className="filtros">
+      <label>Estado:</label>
+      <select value={filtroEstado} onChange={(e) => setFiltroEstado(e.target.value)}>
+        <option value="todas">Todos</option>
+        <option value="Pendiente">Pendiente</option>
+        <option value="Completada">Completada</option>
+      </select>
+    </div>
       <table id="tabla-tareas">
         <thead>
           <tr>
@@ -153,7 +195,6 @@ function App() {
           </tr>
         </thead>
         <tbody>
-          {tareas.length === 0 ? <tr><td colSpan="3">No hay tareas :)</td></tr> : (
             <ListaTareas 
               tareas={tareas} 
               filtroCategoria={filtroCategoria} 
@@ -162,22 +203,22 @@ function App() {
               deleteTask={deleteTask} 
               abrirModal={abrirModal}
             />
-          )}
         </tbody>
       </table>
 
       <FormularioTarea nuevaTarea={nuevaTarea} setNuevaTarea={setNuevaTarea} agregarTarea={(e) => { //Esta es la funcion llamada cuando se hace submit a Agregar Tarea
+        console.log("Agregando tarea: " + nuevaTarea.texto + " con categoria: " + nuevaTarea.categoria + " y estado: " + nuevaTarea.estado);
         e.preventDefault(); // Evitar que el formulario se envíe en el evento actual recargue la página
         if(nuevaTarea.texto.trim() === '') {
           alert('El texto de la tarea no puede estar vacío');
           return;
         }
-        setTareas([...tareas, { id: Date.now(), texto: nuevaTarea.texto, categoria: nuevaTarea.categoria, estado: 'pendiente' }]);
-        setNuevaTarea({ texto: '', categoria: 'personal' });
+        setTareas([...tareas, { id: Date.now(), texto: nuevaTarea.texto, categoria: nuevaTarea.categoria, estado: nuevaTarea.estado }]);
+        setNuevaTarea({...nuevaTarea, texto: '' });
       } } />
 
       {modalAbierto && (
-        <ModalEditarTarea tarea={tareaAEditar} actualizarYCerrarModal={actualizarTarea} />
+        <ModalEditarTarea tarea={tareaAEditar} actualizarYCerrarModal={actualizarTarea} cerrarModal={cerrarModal}/>
       )}
     </>
 
